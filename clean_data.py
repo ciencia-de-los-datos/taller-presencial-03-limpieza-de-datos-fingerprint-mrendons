66bf15a96c5e957e00cda4da723425b33990998f
mrendons <mrendons@unal.edu.co>
2024-03-08 18:54:39 -0500

    intento1

--- clean_data.py
+++ clean_data.py
@@ -2,10 +2,14 @@
 
 import nltk
 import pandas as pd
-
+import string
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
+    df = pd.read_csv(input_file)
+    return df
+
+
 
 
 def create_fingerprint(df):
@@ -21,11 +25,34 @@ def create_fingerprint(df):
     # 8. Ordene la lista de tokens y remueve duplicados
     # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
 
+    df = df.copy() #obligatoria, para no modificar el real
+    df["key"] = df["text"] #nueva columna con el contenido de la columna text
+    df["key"] = df["key"].str.strip() #paso 2
+    df["key"] = df["key"].str.lower() #paso 3
+    df["key"] = df["key"].str.replace("-","") #paso 4
+    df["key"] = df["key"].str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    ) #paso 5
+    df["key"] = df["key"].str.split()#paso 6 lo convierte en una lista con n elementos (palabras)
+    stemmer = nltk.PorterStemmer()
+    df["key"] = df["key"].apply(lambda x:[stemmer.stem(word)for word in x]) #paso 7 transforma las palabras para generar la raiz de la palabra
+    #a la columna key le aplica a cada elemento de la columna la función lambda cuyo variable es x, a cada palabra apliquele el stemmer 
+    df["key"] = df["key"].apply(lambda x:sorted(set(x))) #paso 8 ordena con sorted y set convierte la lista en un conjunto de datos ordenados sin elementos repetidos
+    df["key"] = df["key"].str.join(" ") #paso 9 
+    
+    return df
+
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key",keep="first")
+    key_dict = dict(zip(keys["key"],keys["text"]))#zip aparea. 
+    df["cleaned"] = df["key"].map(key_dict) #Si se mapea un diccionario en una columna, le aplica el diccionario al elemento
+    return df
+
+
 
     # 1. Ordene el dataframe por 'fingerprint' y 'text'
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
@@ -37,7 +64,11 @@ def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
     # Solo contiene una columna llamada 'texto' al igual
     # que en el archivo original pero con los datos limpios
-
+    
+    df = df.copy() #IMPORTANTE
+    df = df[["cleaned"]]
+    df = df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, index=False)
 
 def main(input_file, output_file):
     """Ejecuta la limpieza de datos"""
